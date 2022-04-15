007c819310475f6f2b3488782808c7abb0c2abe6
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2022-04-15 21:04:19 +0200

    refactorización completa

--- scripts.js
+++ scripts.js
@@ -1,48 +1,45 @@
 const input = document.querySelector("input");
 const botonGuardar = document.querySelector(".boton");
-const botonImprimir = document.querySelector(".botonImprimir");
-const dragArea = document.querySelector(".contenedorPrincipal");
+const botonImprimir = document.querySelector("#imprimir");
+const botonLimpiar = document.querySelector("#limpiar");
+const dragArea = document.querySelector("#container");
+const contenedor = document.querySelector(".container");
 
 
 // Función que realiza la animación de los elementos.
 new Sortable(dragArea, {
-  Animation: 350
+  Animation: 100,
 });
 
 // Pregunta si estamos seguros de cerrar la aplicación.
-window.addEventListener("beforeunload", function (event) {
-  event.returnValue = "Salir";
+window.addEventListener("beforeunload", (evento) => {
+  evento.returnValue = "Salir";
 });
 
 // Configuración de los botones
-botonImprimir.addEventListener("click", ()=>{
+botonImprimir.addEventListener("click", () => {
   print();
-})
+});
 
-botonGuardar.addEventListener("click", ()=>{
+botonGuardar.addEventListener("click", () => {
   mostrarMensaje();
-})
+});
 
+botonLimpiar.addEventListener("click", () => {
+  input.value = "";
+})
 
 // Función principal que crea de forma dinámica los elementos.
-function mostrarMensaje() {
-  const nav = document.querySelector("nav");
-
-  const panel = document.createElement("div");
-  nav.appendChild(panel);
-  panel.setAttribute("class", "contenedorVirtual");
-
-  const mensaje = document.createElement("p");
-  mensaje.textContent = input.value;
-  panel.appendChild(mensaje);
-  mensaje.setAttribute("class", "parrafo");
-
-  const botonCerrar = document.createElement("button");
-  botonCerrar.textContent = "x";
-  panel.appendChild(botonCerrar);
-  botonCerrar.setAttribute("class", "botonCerrar");
-
-  botonCerrar.addEventListener("click", ()=>{
-    panel.parentNode.removeChild(panel);
-  })
+let cerrar = () => {
+  const div = document.querySelector(".contenedor__virtual");
+  contenedor.removeChild(div);
 }
+
+const mostrarMensaje = () => {
+  const panel = /*html*/ `<div class="contenedor__virtual">
+                          <p>${input.value}</p>
+                          <button onclick="cerrar()" class="botonCerrar">x</button>
+                         </div>`;
+
+  contenedor.innerHTML += panel;
+};
